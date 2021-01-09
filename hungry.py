e6bcff132ee88a091b25d147d99bf52e310155d5
Sridhar <singamsri@gmail.com>
2021-01-09 14:19:55 +0530

    Changed the program

--- hungry.py
+++ hungry.py
@@ -1,3 +1,6 @@
-Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> print("I am Hungry")
+hungry = input("Are you Hungry?")
+    
+if hungry == "yes":
+    print("Eat Samosa")
+else:
+    print("do your homework")
